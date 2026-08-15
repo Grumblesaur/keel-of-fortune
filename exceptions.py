65cbccb387720249819c8713b629624f5a7353a3
James Murphy <james.frank.murphy@proton.me>
2026-08-15 14:39:15 -0400

    registration code

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+class RandomizerError(Exception):
+    pass
+
+
+class RegistrationError(RandomizerError):
+    pass
+
+
+class UnrecognizedShips(RegistrationError):
+    pass
+
+
+class SpamError(RegistrationError):
